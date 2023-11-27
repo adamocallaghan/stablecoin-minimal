f5d43fa38862f698333e5f7ac270280ad2c5c8e1
adamocallaghan <adam.ocallaghan@yahoo.ie>
2023-11-27 12:29:18 +0000

    Cleaning up depositAndMint function

--- src/StablecoinEngine.sol
+++ src/StablecoinEngine.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {Stablecoin} from "./Stablecoin.sol";
 import {AggregatorV3Interface} from "chainlink/contracts/src/interfaces/AggregatorV3Interface.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
